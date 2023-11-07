0cef226de0612eeb8a5f36939288939eaab1bcde
guiyonglin <guiyonglin@sniffgroup.com>
2023-11-07 16:28:12 +0800

    i18n

---
+++ mercury.config.js
@@ -0,0 +1,30 @@
+
+const path = require('path');
+
+const getExtractConfig = () => {
+    const extractConfig = require('./src/i18n/locales/extractConfig.json');
+    const { common, pages } = extractConfig;
+
+    const wrapConfig = (conf) => {
+        return conf.map((item) => {
+            return {
+                ...item,
+                relativePath: item.path,
+                path: path.join(__dirname, item.path)
+            };
+        });
+    };
+    return {
+        pages: wrapConfig(pages),
+        common: wrapConfig(common)
+    };
+};
+module.exports = {
+    extractConfig: getExtractConfig(),
+    isGeneratorJSON: true,
+    isHandleFile: () => {
+        return true;
+    },
+    generatorStaticDirName: `${path.join(__dirname, './src/i18n/locales')}`,
+    isModifySourceCode: true
+};
